a143b44bd917e2fbc2561fc341fae5c18ba78821
loclink <loclink@163.com>
2024-02-22 00:00:39 +0800

    refactor: 全局命名空间名称修改为TJ

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
 import path from 'path';
+import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 export default defineConfig({
   build: {
     lib: {
       entry: './src/index.ts',
-      name: 'WM',
+      name: 'TJ',
       fileName: (format) => `index.${format === 'umd' ? 'min.js' : 'js'}`
     }
   },

a2260dec3f77e4ddecbd8379dca75ef1c2d26b1f
loclink <loclink@163.com>
2024-02-28 04:15:48 +0800

    test: 测试文档部署

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -4,7 +4,7 @@ import DefaultTheme from 'vitepress/theme';
 export default {
   ...DefaultTheme,
   async enhanceApp() {
-    if ((!import.meta as any).env.SSR) {
+    if ((!import.meta as any)?.env?.SSR) {
       const { loadOml2d } = await import('oh-my-live2d');
       loadOml2d({
         models: [

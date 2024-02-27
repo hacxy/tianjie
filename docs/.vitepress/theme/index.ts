25ed554213925213f7bb2543a034d4f9917058cb
loclink <loclink@163.com>
2024-02-28 04:13:23 +0800

    docs: 文档新增oh-my-live2d依赖

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,23 @@
 // .vitepress/theme/index.js
 import DefaultTheme from 'vitepress/theme';
+
 export default {
   ...DefaultTheme,
-  // enhanceApp(ctx) {
-  //   DefaultTheme.enhanceApp(ctx);
-  //   // ...
-  // },
-  setup() {
-    console.log('--------------------------');
+  async enhanceApp() {
+    if ((!import.meta as any).env.SSR) {
+      const { loadOml2d } = await import('oh-my-live2d');
+      loadOml2d({
+        models: [
+          {
+            path: 'https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json',
+            scale: 0.12,
+            position: [-10, 50],
+            stageStyle: {
+              width: 350
+            }
+          }
+        ]
+      });
+    }
   }
 };
